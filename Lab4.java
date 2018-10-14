29b7b49448aa75154e9ea9f975c32270bf7aa528
John <johncgumm@gmail.com>
2018-10-14 09:34:01 -0700

    Program finished with continue loops and array choice

--- Lab4.java
+++ Lab4.java
@@ -1,8 +1,8 @@
-/*
+/*********************************************
 **  John G
 **  CIS 131
 **  Lab 4
-*/
+*********************************************/
 
 public class Lab4 {
 
@@ -10,116 +10,157 @@ public class Lab4 {
     private static final int ARRAY_SIZE = 10;
     private static final int RAND_LOW = 1;
     private static final int RAND_HIGH = 300;
+    private static final int QUIT = 0;
+    private static final int ARRAY_1D = 1;
+    private static final int ARRAY_2D = 2;
 
     public static void main(String args[]) { // -------- Main -------------------//
 
-        // Declares an array of 10 ints
-        int[] myArray = new int[ARRAY_SIZE];
-
-        // Declare vars
-        int largestIndex;
-        int largestValue;
-        int smallestIndex;
-        int smallestValue;
-        int sum;
-        double average;
-
-        // Create and display array
-        populateArray(myArray);
-        System.out.println("Before the Sorting:");
-        displayArray(myArray);
-
-        // Calculate smallest/largest index
-        largestIndex = getLargeIndex(myArray);
-        largestValue = myArray[largestIndex];
-        smallestIndex = getSmallIndex(myArray);
-        smallestValue = myArray[smallestIndex];
-
-        // Calculate sum/average
-        sum = calculateSum(myArray);
-        average = calculateAverage(sum);
-
-        // Display calculations
-        displayCalculations(largestIndex, largestValue, smallestIndex, smallestValue, sum, average);
-
-        // Sort then display array
-        sortArray(myArray);
-        System.out.println("After the sorting:");
-        displayArray(myArray);
-
-        //--------------------------2D ARRAY ------------------------------------//
-
-        System.out.println("********************************************************************************");
-        System.out.println("                                    2D ARRAY");
-        System.out.println("********************************************************************************");
-
-        // Declares 2d array of 10 x 10 ints
-        int[][] my2dArray = new int[ARRAY_SIZE][ARRAY_SIZE];
-
-        // Declare vars
-        int largest2dIndexR;
-        int largest2dIndexC;
-        int largest2dValue;
-        int smallest2dIndexR;
-        int smallest2dIndexC;
-        int smallest2dValue;
-        int sum2d;
-        double average2d;
-
-        // Create and display array
-        populateArray(my2dArray);
-        System.out.println("Before the Sorting:");
-        displayArray(my2dArray);
-
-        // Calculate largest
-        largest2dIndexR = getLargeIndexR(my2dArray);
-        largest2dIndexC = getLargeIndexC(my2dArray);
-        largest2dValue = my2dArray[largest2dIndexR][largest2dIndexC];
-
-        // Calculate smallest
-        smallest2dIndexR = getSmallIndexR(my2dArray);
-        smallest2dIndexC = getSmallIndexC(my2dArray);
-        smallest2dValue = my2dArray[smallest2dIndexR][smallest2dIndexC];
-
-        // Sum/Average
-        sum2d = calculateSum(my2dArray);
-        average2d = calculateAverage2d(sum2d);
-
-        // Display Calculations
-        displayCalculations(largest2dIndexR, largest2dIndexC, largest2dValue, smallest2dIndexR, smallest2dIndexC, smallest2dValue, sum2d, average2d);
-        System.out.println();
-
-        // Create 1d array for sorting
-        int[] sortingArray = new int[ARRAY_SIZE * ARRAY_SIZE];
+        // Get value from user on which array to use
+        int userChoice = getArrayChoice("How many dimensions would you like the array to be?\nPlease enter 1, 2, or 0 to quit.");
         
-        // For-loop 2d array into new 1d array
-        int sortingArrayIndex = 0;
-        for (int i = 0; i < my2dArray.length; i++) {
-            for (int j = 0; j < my2dArray[i].length; j++) {
-                sortingArray[sortingArrayIndex] = my2dArray[i][j];
-                sortingArrayIndex++;
-            }
-        }
+        while (userChoice != QUIT) { // Start of while-not-sentinel loop
 
-        // Sort 1d array using insertion sort
-        sortArray(sortingArray);
+            //--------------------------1D ARRAY ------------------------------------//
 
-        // For-loop 1d array back into 2d array
-        int index = 0;
-        for (int i = 0; i < ARRAY_SIZE; i++) {
-            for (int j = 0; j < ARRAY_SIZE; j++) {
-                my2dArray[i][j] = sortingArray[index];
-                index++;
-            }
-        }
+            if (userChoice == ARRAY_1D) { // Start of 1D array loop
 
-        // Display sorted 2d array
-        System.out.println("After the sorting:");
-        displayArray(my2dArray);
+                System.out.println("********************************************************************************");
+                System.out.println("                                    1D ARRAY");
+                System.out.println("********************************************************************************\n");
 
+                // Declares an array of 10 ints
+                int[] myArray = new int[ARRAY_SIZE];
+
+                // Declare vars
+                int largestIndex;
+                int largestValue;
+                int smallestIndex;
+                int smallestValue;
+                int sum;
+                double average;
+
+                // Create and display array
+                populateArray(myArray);
+                System.out.println("Before the Sorting:");
+                displayArray(myArray);
+
+                // Calculate smallest/largest index
+                largestIndex = getLargeIndex(myArray);
+                largestValue = myArray[largestIndex];
+                smallestIndex = getSmallIndex(myArray);
+                smallestValue = myArray[smallestIndex];
+
+                // Calculate sum/average
+                sum = calculateSum(myArray);
+                average = calculateAverage(sum);
+
+                // Display calculations
+                displayCalculations(largestIndex, largestValue, smallestIndex, smallestValue, sum, average);
+
+                // Sort then display array
+                sortArray(myArray);
+                System.out.println("After the sorting:");
+                displayArray(myArray);
+
+                // Get user choice again
+                userChoice = getArrayChoice("How many dimensions would you like the array to be?\nPlease enter 1, 2, or 0 to quit.");
+
+            } // End of 1D array loop
+
+            //--------------------------2D ARRAY ------------------------------------//
+
+            if (userChoice == ARRAY_2D) { // Start of 2D array loop
+
+                System.out.println("********************************************************************************");
+                System.out.println("                                    2D ARRAY");
+                System.out.println("********************************************************************************\n");
+
+                // Declares 2d array of 10 x 10 ints
+                int[][] my2dArray = new int[ARRAY_SIZE][ARRAY_SIZE];
+
+                // Declare vars
+                int largest2dIndexR;
+                int largest2dIndexC;
+                int largest2dValue;
+                int smallest2dIndexR;
+                int smallest2dIndexC;
+                int smallest2dValue;
+                int sum2d;
+                double average2d;
+
+                // Create and display array
+                populateArray(my2dArray);
+                System.out.println("Before the Sorting:");
+                displayArray(my2dArray);
+
+                // Calculate largest
+                largest2dIndexR = getLargeIndexR(my2dArray);
+                largest2dIndexC = getLargeIndexC(my2dArray);
+                largest2dValue = my2dArray[largest2dIndexR][largest2dIndexC];
+
+                // Calculate smallest
+                smallest2dIndexR = getSmallIndexR(my2dArray);
+                smallest2dIndexC = getSmallIndexC(my2dArray);
+                smallest2dValue = my2dArray[smallest2dIndexR][smallest2dIndexC];
+
+                // Sum/Average
+                sum2d = calculateSum(my2dArray);
+                average2d = calculateAverage2d(sum2d);
+
+                // Display Calculations
+                displayCalculations(largest2dIndexR, largest2dIndexC, largest2dValue, smallest2dIndexR, smallest2dIndexC, smallest2dValue, sum2d, average2d);
+                System.out.println();
+
+                // Create 1d array for sorting
+                int[] sortingArray = new int[ARRAY_SIZE * ARRAY_SIZE];
+                
+                // For-loop 2d array into new 1d array
+                int sortingArrayIndex = 0;
+                for (int i = 0; i < my2dArray.length; i++) {
+                    for (int j = 0; j < my2dArray[i].length; j++) {
+                        sortingArray[sortingArrayIndex] = my2dArray[i][j];
+                        sortingArrayIndex++;
+                    }
+                }
+
+                // Sort 1d array using insertion sort
+                sortArray(sortingArray);
+
+                // For-loop 1d array back into 2d array
+                int index = 0;
+                for (int i = 0; i < ARRAY_SIZE; i++) {
+                    for (int j = 0; j < ARRAY_SIZE; j++) {
+                        my2dArray[i][j] = sortingArray[index];
+                        index++;
+                    }
+                }
+
+                // Display sorted 2d array
+                System.out.println("After the sorting:");
+                displayArray(my2dArray);
+
+                // Get user choice again
+                userChoice = getArrayChoice("How many dimensions would you like the array to be?\nPlease enter 1, 2, or 0 to quit.");
+            
+            } // End of 2D array loop 
+
+        } // End of while-not-sentinel loop
+
+        System.out.println("Program ending normally...");
 
     } // --------------------------------------------- End Main ----------------//
 
+    // Input validation for array selection
+    public static int getArrayChoice(String msg) {
+        int choice = IR4.getInteger(msg);
+        while (choice < QUIT || choice > ARRAY_2D) {
+            System.out.println("Error: Please enter a valid integer.");
+            choice = IR4.getInteger(msg);
+        }
+
+        return choice;
+    }
     
     // Display array elements with single space between elements
     public static void displayArray(int[] array) {
